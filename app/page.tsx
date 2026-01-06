8ee968e96dfdb751e4898983bf758ee4244e67a6
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2026-01-06 22:42:57 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <Button className="bg-red-400"> subscribe</Button>
+      <Header />
+      <Hero />
     </div>
   );
 }
