6e57a225368ba2ed4014ed50cbf50b2c53a2ad9a
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2026-01-07 14:01:09 +0530

    add header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,51 @@
+import Image from "next/image";
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="bg-black">
+      <div className="mx-auto flex h-16 max-w-7xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+        <Image src="/logo.svg" alt="logo" width={100} height={100} />
+
+        <div className="flex flex-1 items-center justify-end md:justify-between">
+          <nav aria-label="Global" className="hidden md:block">
+            <ul className="flex items-center gap-6 text-sm">
+              {["About", "Careers", "History", "Services", "Projects"].map(
+                (item) => (
+                  <li key={item}>
+                    <a
+                      className="text-white transition hover:text-gray-100/75"
+                      href="#"
+                    >
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
+          </nav>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:flex sm:gap-4">
+              <a
+                className="block rounded-md px-5 py-2.5 text-sm font-medium text-white transition"
+                href="#"
+              >
+                Login
+              </a>
+
+              <a
+                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-black transition hover:text-slate-800 sm:block"
+                href="#"
+              >
+                Register
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="bg-black lg:grid lg:h-screen lg:place-content-center">
+      <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+        <div className="mx-auto max-w-prose text-center">
+          <h1 className="text-4xl font-bold text-sky-300 sm:text-5xl dark:text-white">
+            Understand user flow and
+            <strong className="text-sky-300"> increase </strong>
+            conversions
+          </h1>
+
+          <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed dark:text-slate-200">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque,
+            nisi. Natus, provident accusamus impedit minima harum corporis
+            iusto.
+          </p>
+
+          <div className="mt-4 flex justify-center gap-4 sm:mt-6">
+            <a
+              className="inline-block rounded  bg-white px-5 py-3 font-medium text-black shadow-sm transition-colors hover:bg-slate-300"
+              href="#"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
