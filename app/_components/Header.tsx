d02cee98252d1723d4153f4660baa0f26ab7e275
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2026-01-19 14:54:24 +0530

    add auth

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,3 +1,4 @@
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
 import Image from "next/image";
 import React from "react";
 
@@ -20,7 +21,7 @@ function Header() {
                       {item}
                     </a>
                   </li>
-                )
+                ),
               )}
             </ul>
           </nav>
@@ -31,14 +32,14 @@ function Header() {
                 className="block rounded-md px-5 py-2.5 text-sm font-medium text-white transition"
                 href="#"
               >
-                Login
+                <LoginLink postLoginRedirectURL="/dashboard">Login</LoginLink>
               </a>
 
               <a
                 className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-black transition hover:text-slate-800 sm:block"
                 href="#"
               >
-                Register
+                <RegisterLink>Register</RegisterLink>
               </a>
             </div>
           </div>
